70d75f2abc77e267fcf5e3b320ce30c4590d7bf3
Felipe <fae.20@hotmail.com>
2018-08-14 13:25:34 -0300

    Implement Material design

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from '@material-ui/core/Button';
 
+const App = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<App />, document.getElementById("index"));
