b520acf05dc7fc88f4a28a7861b8cb3e0fdb9592
Felipe <fae.20@hotmail.com>
2018-08-28 18:12:39 -0300

    Config estructure Component Bookcard

--- new-structure/src/components/BookCard.js
+++ new-structure/src/components/BookCard.js
@@ -2,19 +2,24 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import StarAvaliation from './StarAvaliation';
 
-class BookTitle extends Component {
+class BookCard extends Component {
 
   render(){    
     
     return (    
       <Grid item xs={6} sm={6}>
-        <Card>                  
-          <figure>
-            <img src={this.props.volumeInfo.imageLinks.thumbnail} alt={this.props.volumeInfo.title}/>
-          </figure>
-          <CardContent>
+        <Card>      
+          <StarAvaliation />
+          <CardMedia
+            image={this.props.volumeInfo.imageLinks.thumbnail}
+            title={this.props.volumeInfo.title}
+          />          
+          <img src={this.props.volumeInfo.imageLinks.thumbnail} alt={this.props.volumeInfo.title}/>
+          <CardContent className="title">
             <Typography gutterBottom variant="headline" component="h2">
               {this.props.volumeInfo.title}
             </Typography>                                    
@@ -25,4 +30,4 @@ class BookTitle extends Component {
   }
 }
 
-export default BookTitle;
+export default BookCard;
