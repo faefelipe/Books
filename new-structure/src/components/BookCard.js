e0e749baa75b37275d2b601ef4c084441130fc91
Felipe <fae.20@hotmail.com>
2018-08-28 15:23:59 -0300

    Config structure component BookCard[

--- new-structure/src/components/BookCard.js
+++ new-structure/src/components/BookCard.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-class BookCard extends Component {
-  render() {
-    return (
-      <div className="App">
-      BookCard
-      </div>
-    );
+class BookTitle extends Component {
+
+  render(){    
+    
+    return (    
+      <Grid item xs={6} sm={6}>
+        <Card>                  
+          <figure>
+            <img src={this.props.volumeInfo.imageLinks.thumbnail} alt={this.props.volumeInfo.title}/>
+          </figure>
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              {this.props.volumeInfo.title}
+            </Typography>                                    
+          </CardContent>          
+        </Card>
+      </Grid>       
+    )
   }
 }
 
-export default BookCard;
+export default BookTitle;
