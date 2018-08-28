7eb4da75992c6033e38f00b0b8e450bd5db74285
Felipe <fae.20@hotmail.com>
2018-08-28 15:22:45 -0300

    Config function onSearch and state Books

--- new-structure/src/container/Book.js
+++ new-structure/src/container/Book.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Search from '../components/Search';
-import ListBooks from '../components/ListBooks'
+import ListBooks from '../components/ListBooks';
 
 class Book extends Component {
   constructor(props){
     super(props);
-    this.state = {value: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);    
+    this.state = {
+      books: [] 
+    };            
   }
+  
   onSearch(term){
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}`, {
         method: "GET",
@@ -22,7 +23,7 @@ class Book extends Component {
     return (
       <div className="App">
         <Search onSearch={this.onSearch.bind(this)} />
-        <ListBooks />        
+        <ListBooks books={this.state.books} />        
       </div>
     );
   }
