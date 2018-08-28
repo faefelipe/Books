382d089fb820ef5f757e08294adaf8e0307cd9ac
Felipe <fae.20@hotmail.com>
2018-08-28 15:25:17 -0300

    Insert function map in component ListBooks

--- new-structure/src/components/ListBooks.js
+++ new-structure/src/components/ListBooks.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import BookCard from './BookCard';
 
-class ListBooks extends Component {
+class ListBooks extends Component {  
+  constructor(props){
+    super(props);    
+    this.state = {};
+  } 
+
   render() {
+    const {books} = this.props
     return (
       <div className="App">
-        <BookCard />
+        <div style={{display: 'flex', flexWrap: 'wrap', width: '900px', margin: 'auto' }}>          
+          {books.map(
+              book => <BookCard key={book.id} {...book} />
+            )
+          }
+        </div> 
       </div>
     );
   }
