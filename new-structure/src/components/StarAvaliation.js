1c870d48f9db1645d5dfaebef70d228ad936b6d5
Felipe <fae.20@hotmail.com>
2018-08-29 11:55:10 -0300

    Create component StarAvaliation and config

--- new-structure/src/components/StarAvaliation.js
+++ new-structure/src/components/StarAvaliation.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-import styles from '../styles/StarAvaliation.css';
+import '../styles/StarAvaliation.css';
 
-const qtdStar = [1,2,3,4,5];
-  const StarAvaliation = (props) => {
+  const StarAvaliation = (props) => {    
+    
     return (
-      <ul>
-        {qtdStar.map(i => {
-          return <FontAwesomeIcon className={styles.star} key={i} icon={faStar} />
-        })}
-      </ul>
+      <li>
+        <FontAwesomeIcon className="star" icon={faStar} />
+      </li>
     )
   }    
   
